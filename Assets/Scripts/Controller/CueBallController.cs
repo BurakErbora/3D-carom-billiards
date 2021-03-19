a05179141c6dbda03dceeec3eda972e701b96e69
A. Burak Erbora <burak.erbora@gmail.com>
2021-03-19 10:56:14 +0100

    Added a Cue Ball controller and physic matierial (for wall bounces)

---
+++ Assets/Scripts/Controller/CueBallController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CueBallController : MonoBehaviour
+{
+    [Header("Hit Force")]
+    public float forceTimerTick = 0.5f;
+    public float baseForce = 500f;
+    public float maximumForceTicks = 10;
+
+    private Rigidbody _ballRB;
+    private Transform _cameraTransform;
+    private float _currentHitForceScale;
+    private float _lastHitForceScaleTime;
+    
+    
+
+    private void Awake()
+    {
+        _ballRB = GetComponent<Rigidbody>();
+        _cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        // Track space keypress. When space is held, scale the to-be-applied force with the hit force tick setting.
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            if (_lastHitForceScaleTime == -1)
+                _lastHitForceScaleTime = Time.timeSinceLevelLoad;
+
+            else if (Time.timeSinceLevelLoad - _lastHitForceScaleTime >= forceTimerTick)
+            {
+                _currentHitForceScale = Mathf.Min(_currentHitForceScale + 1, maximumForceTicks);
+                _lastHitForceScaleTime = Time.timeSinceLevelLoad;
+                Debug.Log("Force increased to " + _currentHitForceScale); // TO-DO: update GUI
+            }
+                
+        }
+        // when released, apply the final force
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _ballRB.AddForce(CalculateHitForce());
+            _currentHitForceScale = 0f;
+            _lastHitForceScaleTime = -1;
+        }
+
+    }
+
+    private Vector3 CalculateHitForce()
+    {
+        return _cameraTransform.forward * _currentHitForceScale * baseForce;
+    }
+}
